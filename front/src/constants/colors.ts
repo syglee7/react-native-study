6a53fecab74082b5822590b7f72339658c8f1c10
iseon-yeong <syglee7@gmail.com>
2024-10-12 14:28:45 +0900

    로그인 화면 구현

--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -1,7 +1,13 @@
 const colors = {
   WHITE: '#FFFFFF',
+  BLACK: '#000000',
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#828282',
+  GRAY_700: '#575757',
   PINK_500: '#BF5C79',
   PINK_700: '#C63B64',
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
 };
 
 export {colors};
