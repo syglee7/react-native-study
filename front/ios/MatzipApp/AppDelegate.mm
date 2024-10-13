e30793cd82a25454badcd20a9c8868eaa1f92502
iseon-yeong <syglee7@gmail.com>
2024-10-13 23:38:21 +0900

    구글맵 api 연동

--- front/ios/MatzipApp/AppDelegate.mm
+++ front/ios/MatzipApp/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+ [GMSServices provideAPIKey:@"AIzaSyB3u0u6JuTRB31XmwqIIigDSysvTE7kk_k"]; // add this line using the api key obtained from Google Console
   self.moduleName = @"MatzipApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
