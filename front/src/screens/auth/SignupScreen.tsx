3727205aefe8be837b50156194bba9c6d8228a80
iseon-yeong <syglee7@gmail.com>
2024-10-13 11:37:15 +0900

    회원가입 스크린과 validate

--- front/src/screens/auth/SignupScreen.tsx
+++ front/src/screens/auth/SignupScreen.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import InputField from '../../components/InputField.tsx';
+import useForm from '../../hooks/useForm.ts';
+import CustomButton from '../../components/CustomButton.tsx';
+import {validateSignup} from '../../utils';
 
 function SignupScreen() {
+  const signup = useForm({
+    initialValue: {email: '', password: '', passwordConfirm: ''},
+    validate: validateSignup,
+  });
   return (
-    <View>
-      <Text>회원가입</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={signup.errors.email}
+          touched={signup?.touched.email}
+          inputMode="email"
+          {...signup.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={signup.errors.password}
+          touched={signup.touched.password}
+          secureTextEntry
+          {...signup.getTextInputProps('password')}
+        />
+        <InputField
+          placeholder="비밀번호 확인"
+          error={signup.errors.password}
+          touched={signup.touched.password}
+          secureTextEntry
+          {...signup.getTextInputProps('passwordConfirm')}
+        />
+      </View>
+      <CustomButton label="회원가입" />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default SignupScreen;
