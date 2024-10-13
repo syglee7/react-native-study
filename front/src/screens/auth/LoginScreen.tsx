3dfe7579dc24bba642149ee90d70d6afdd0e0588
iseon-yeong <syglee7@gmail.com>
2024-10-13 16:50:41 +0900

    폼 사용성 개선하기

--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useRef} from 'react';
+import {SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
 import InputField from '../../components/InputField.tsx';
 import CustomButton from '../../components/CustomButton.tsx';
 import useForm from '../../hooks/useForm.ts';
@@ -41,6 +41,8 @@ function LoginScreen() {
   //   });
   // };
 
+  const passwordRef = useRef<TextInput | null>(null);
+
   const login = useForm({
     initialValue: {email: '', password: ''},
     validate: validateLogin,
@@ -54,20 +56,28 @@ function LoginScreen() {
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <InputField
+          autoFocus
           placeholder="이메일"
           error={login.errors.email}
           touched={login?.touched.email}
           inputMode="email"
+          blurOnSubmit={false}
+          returnKeyType="next"
+          onSubmitEditing={() => passwordRef.current?.focus()}
           // value={values.email}
           // onChangeText={text => handleChangeText('email', text)}
           // onBlur={() => handleBlur('email')}
           {...login.getTextInputProps('email')}
         />
         <InputField
+          ref={passwordRef}
           placeholder="비밀번호"
           error={login.errors.password}
           touched={login.touched.password}
           secureTextEntry
+          blurOnSubmit={false}
+          returnKeyType="join"
+          onSubmitEditing={handleSubmit}
           // value={values.password}
           // onChangeText={text => handleChangeText('password', text)}
           // onBlur={() => handleBlur('password')}
--- front/src/utils/index.ts
+++ front/src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './validate.ts';
+export * from './common.ts';
